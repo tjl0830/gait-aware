cc47782e52b15bb3ab5ac2e74cb01f67a532659b
James Bautista <tjlbautista30@gmail.com>
2025-10-20 00:46:05 +0800

    feat(ui): add user information form section to index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
 import { useVideoPlayer } from 'expo-video';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { VideoPicker } from '../../components/VideoPicker';
 import { VideoPreview } from '../../components/VideoPreview';
 import { useVideoPickerLogic } from '../../components/hooks/useVideoPickerLogic';
+import UserInfo from '../user_info';
 
 export default function Tab() {
   const { videoUri, fileName, pickVideo } = useVideoPickerLogic();
@@ -17,24 +18,40 @@ export default function Tab() {
   });
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.stepTitle}>Step 1: Upload Video</Text>
-      <VideoPicker onPress={pickVideo} />
-      <VideoPreview uri={videoUri} fileName={fileName} player={player} />
-    </View>
+    <ScrollView style={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.videoContainer}>
+          <Text style={styles.stepTitle}>Step 1: Upload Video</Text>
+          <VideoPicker onPress={pickVideo} />
+          <VideoPreview uri={videoUri} fileName={fileName} player={player} />
+        </View>
+        <UserInfo />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+  },
   container: {
     padding: 32,
-    flex: 1,
-    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   stepTitle: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 16,
+  },
+  videoContainer: {
+    width: '100%',
+    alignSelf: 'center',
+    alignItems: 'center', // Add this to center children horizontally
+    marginTop: 20,
+    maxWidth: 600,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 8,
   },
 });
